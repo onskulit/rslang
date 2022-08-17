db14f2ab7debaa7e8f794aa97d90eb9bda03b9f4
onskulit <dislaty@mail.com>
2022-08-17 16:01:13 +0300

    refactor: updated Header name and delete redundant className

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,14 +1,13 @@
 import Nav from './nav/Nav';
 import { PageHeader } from 'antd';
 
-function _Header() {
+function Header() {
   return (
     <PageHeader
-      className="site-page-header"
       title="RSLang Team 43" 
       extra={<Nav />}
     />
   );
 }
 
-export default _Header;
+export default Header;
