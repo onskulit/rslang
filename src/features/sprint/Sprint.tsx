9339292e465670200819524955a0a34759c0db5a
onskulit <dislaty@mail.com>
2022-08-18 15:33:47 +0300

    feat: add first action creator

--- src/features/sprint/Sprint.tsx
+++ src/features/sprint/Sprint.tsx
@@ -1,5 +1,20 @@
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { RootState } from '../../app/store';
+import { sprintSlice } from './sprintSlice';
+
 function Sprint() {
-  return <div>Sprint</div>;
+  const { addWord } = sprintSlice.actions;
+  const { words } = useAppSelector((state: RootState) => state.sprint);
+  const dispatch = useAppDispatch();
+  return (
+    <div>
+      {words}
+      <div>
+        <button onClick={() => dispatch(addWord('Hello'))}>Верно</button>
+        <button>Неверно</button>
+      </div>
+    </div>
+  );
 }
 
 export default Sprint;
--- src/features/sprint/sprintSlice.ts
+++ src/features/sprint/sprintSlice.ts
@@ -1,16 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
 interface SprintState {
   words: string[];
+  isLoading: boolean;
+  error: string;
 }
 
-const initialState = {
+const initialState: SprintState = {
   words: [],
+  isLoading: false,
+  error: '',
 };
 
 export const sprintSlice = createSlice({
   name: 'sprint',
   initialState,
-  reducers: {},
+  reducers: {
+    addWord(state, action: PayloadAction<string>) {
+      state.words.push(action.payload);
+    },
+  },
 });
 
 export default sprintSlice.reducer;
