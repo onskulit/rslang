4309e97a98399f40563c64f2e29852273c0f0791
Andrey Terekhov <satyr663@gmail.com>
2022-08-29 10:31:32 +0300

    feat: add dropdown menu with games to header

--- src/pages/header/nav/Nav.tsx
+++ src/pages/header/nav/Nav.tsx
@@ -1,14 +1,37 @@
-import styles from './Nav.module.css';
+import { DownOutlined } from '@ant-design/icons';
 import { NavLink } from 'react-router-dom';
+import { Dropdown, Menu, Space } from 'antd';
+import styles from './Nav.module.css';
 
 function Nav() {
+  const menu = (
+    <Menu
+      items={[
+        {
+          label: <NavLink to="/sprint">Спринт</NavLink>,
+          key: '1',
+        },
+        {
+          label: <NavLink to="/audition">Аудиовызов</NavLink>,
+          key: '2',
+        },
+      ]}
+    />
+  );
+
   return (
     <nav className={styles.nav}>
-      <NavLink to="/">Main Page</NavLink>
-      <NavLink to="/textbook">Textbook</NavLink>
-      <NavLink to="/sprint">Sprint</NavLink>
-      <NavLink to="/audition">Audiocall</NavLink>
-      <NavLink to="/statistics">Statistics</NavLink>
+      <NavLink to="/">Главная</NavLink>
+      <NavLink to="/textbook">Учебник</NavLink>
+      <Dropdown overlay={menu}>
+        <a onClick={(e) => e.preventDefault()}>
+          <Space>
+            Игры
+            <DownOutlined />
+          </Space>
+        </a>
+      </Dropdown>
+      <NavLink to="/statistics">Статистика</NavLink>
     </nav>
   );
 }
