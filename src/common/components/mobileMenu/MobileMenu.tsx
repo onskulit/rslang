2a9d57babadfce5772bc270e5900498fd0ab815e
Andrey Terekhov <satyr663@gmail.com>
2022-09-02 15:49:13 +0300

    feat: add links to Authorization to mobile menu

--- src/common/components/mobileMenu/MobileMenu.tsx
+++ src/common/components/mobileMenu/MobileMenu.tsx
@@ -3,12 +3,23 @@ import { Button, Divider, Popover, Grid } from 'antd';
 import { NavLink } from 'react-router-dom';
 import { HEADER_LINKS } from '../../constants/headerLinks';
 import styles from './MobileMenu.module.css';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { storage } from '../../../utils/localStorage';
+import { changeValidation } from '../../../app/reducers/userSlice';
+import { STORAGE_KEY } from '../../constants/localStorage';
 
 const { useBreakpoint } = Grid;
 
 function MobileMenu() {
   const [isVisible, setIsVisible] = useState(false);
+  const { validate } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
   const screens = useBreakpoint();
+  const handleLogIn = () => {
+    dispatch(changeValidation(false));
+    storage.remove(STORAGE_KEY.userAuthData);
+    storage.remove(STORAGE_KEY.userLogInData);
+  };
 
   const handleVisibleChange = (newVisible: boolean) => {
     setIsVisible(newVisible);
@@ -18,10 +29,9 @@ function MobileMenu() {
     () => (
       <>
         {Object.values(HEADER_LINKS).map((link, index) => (
-          <>
+          <div className={styles.menuItem} key={index}>
             <NavLink
               to={link.path}
-              key={index}
               className={styles.link}
               onClick={() => {
                 setIsVisible(false);
@@ -30,11 +40,32 @@ function MobileMenu() {
               {link.name}
             </NavLink>
             <Divider className={styles.divider} />
-          </>
+          </div>
         ))}
+        {validate ? (
+          <a
+            onClick={() => {
+              setIsVisible(false);
+              handleLogIn();
+            }}
+            className={styles.link}
+          >
+            Выход
+          </a>
+        ) : (
+          <NavLink
+            to="/authorization"
+            className={styles.link}
+            onClick={() => {
+              setIsVisible(false);
+            }}
+          >
+            Вход
+          </NavLink>
+        )}
       </>
     ),
-    []
+    [validate]
   );
 
   return (
